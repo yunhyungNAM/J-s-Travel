4aee56de2a4c6b23bc78324b7620bd92c5c239b5
yunhyungNAM <iys04003@naver.com>
2026-01-14 20:45:53 +0900

    [CHORE]-Initial Spring Boot project setup

---
+++ src/main/java/com/YH/travelaccount/TravelAccountApplication.java
@@ -0,0 +1,13 @@
+package com.YH.travelaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAccountApplication.class, args);
+	}
+
+}
