88f7c5c2c117d978610aa9f935632088f037a1f4
yunhyungNAM <iys04003@naver.com>
2026-01-24 18:52:32 +0900

    [CHORE]-backend/frontend 디렉토리 분리 및 Vue 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/YH/travelaccount/expense/controller/ExpenseController.java
@@ -0,0 +1,8 @@
+package com.YH.travelaccount.expense.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+    // TODO: 지출 관련 API 구현
+}
